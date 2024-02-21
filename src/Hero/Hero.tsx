f91a87a5beb09b56f429e86745eb1b0cf7f7b49b
Andy Uyeda <andyjuyeda2@gmail.com>
2024-02-21 00:13:00 -0800

    update hero banner date

--- src/Hero/Hero.tsx
+++ src/Hero/Hero.tsx
@@ -30,7 +30,7 @@ function HeroAlert() {
       </svg>
 
       <AlertTitle className="font-bold">
-        SCHEDULE UPDATE - FEBRUARY 19
+        SCHEDULE UPDATE - FEBRUARY 20
       </AlertTitle>
       <AlertDescription>
         There have been updates to some events. Please check the schedule for
